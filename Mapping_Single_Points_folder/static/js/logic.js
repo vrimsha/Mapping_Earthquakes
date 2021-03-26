4910133c5f88baedfc305bbddd86c4d8d95fe88e
Veronika R <veronikars7@Veronikas-iMac.local>
2021-03-26 19:58:54 -0400

    Uploading logic.js file

---
+++ Mapping_Single_Points_folder/static/js/logic.js
@@ -0,0 +1,2 @@
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -13,3 +13,13 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle tp the map
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
